2c0578fa483008e5f01bfd28b679e35c4f721c55
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-15 18:39:50 +0200

    add class-teacher schema

--- src/class_teacher/class_teacher.module.ts
+++ src/class_teacher/class_teacher.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { ClassTeacherService } from './class_teacher.service';
-import { ClassTeacherController } from './class_teacher.controller';
+import { Module } from "@nestjs/common";
+import { ClassTeacherService } from "./class_teacher.service";
+import { ClassTeacherController } from "./class_teacher.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ClassTeacher, ClassTeacherSchema } from "./schema/class-teacher.schema";
+import { Teacher, TeacherSchema } from "../teacher/schema/teacher.schema";
+import { Classname, ClassnameSchema } from "../classname/schema/classname.schema";
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: ClassTeacher.name, schema: ClassTeacherSchema },
+    { name: Teacher.name, schema: TeacherSchema },
+    { name: Classname.name, schema: ClassnameSchema }
+  ])],
   providers: [ClassTeacherService],
   controllers: [ClassTeacherController]
 })
-export class ClassTeacherModule {}
+export class ClassTeacherModule {
+}
