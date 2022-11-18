3cca8129294ffd2b8e78dbcbed7724b7cf0884fd
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-18 06:07:57 +0200

    add year field

--- src/head_of_department/schema/hod.schema.ts
+++ src/head_of_department/schema/hod.schema.ts
@@ -1,13 +1,18 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose from "mongoose";
 import { Teacher } from "../../teacher/schema/teacher.schema";
 import { Departments } from "../../departments/schema/departments.schema";
 
 @Schema()
-export class HodSchema {
+export class Hod {
   @Prop({ type: mongoose.Schema.Types.ObjectId, name: Teacher.name })
   teacher_id: Teacher;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, name: Departments.name })
   department_id: Departments;
+
+  @Prop()
+  year: string;
 }
+
+export const HodSchema = SchemaFactory.createForClass(Hod);
