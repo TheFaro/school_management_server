83f2cf88dc1c990826be573ee73ea79bdebbf3ae
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-18 20:46:18 +0200

    append subject teacher schema

--- src/subject_teacher/subject_teacher.module.ts
+++ src/subject_teacher/subject_teacher.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { SubjectTeacherController } from './subject_teacher.controller';
-import { SubjectTeacherService } from './subject_teacher.service';
+import { Module } from "@nestjs/common";
+import { SubjectTeacherController } from "./subject_teacher.controller";
+import { SubjectTeacherService } from "./subject_teacher.service";
+import { SubjectTeacher, SubjectTeacherSchema } from "./schema/subject-teacher.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: SubjectTeacher.name, schema: SubjectTeacherSchema }])],
   controllers: [SubjectTeacherController],
   providers: [SubjectTeacherService]
 })
-export class SubjectTeacherModule {}
+export class SubjectTeacherModule {
+}
