1baab87297ab55a81b63fc1edb663673a31eb591
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-18 19:56:52 +0200

    append subject schema

--- src/subjects/subjects.module.ts
+++ src/subjects/subjects.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { SubjectsService } from './subjects.service';
-import { SubjectsController } from './subjects.controller';
+import { Module } from "@nestjs/common";
+import { SubjectsService } from "./subjects.service";
+import { SubjectsController } from "./subjects.controller";
+import { Subjects, SubjectsSchema } from "./schema/subjects.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Subjects.name, schema: SubjectsSchema }])],
   providers: [SubjectsService],
   controllers: [SubjectsController]
 })
-export class SubjectsModule {}
+export class SubjectsModule {
+}
