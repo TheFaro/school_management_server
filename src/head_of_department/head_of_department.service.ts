92ab9307132f18b7dcdc05defaefd12565c9a8ed
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-18 06:07:22 +0200

    add hod service

--- src/head_of_department/head_of_department.service.ts
+++ src/head_of_department/head_of_department.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Hod } from "./schema/hod.schema";
+import { Model } from "mongoose";
+import { HodDto } from "./dto/hod.dto";
 
 @Injectable()
-export class HeadOfDepartmentService {}
+export class HeadOfDepartmentService {
+  constructor(@InjectModel(Hod.name) private readonly hodModel: Model<Hod>) {
+  }
+
+  async registerHod(body: HodDto): Promise<any> {
+    const checking = await this.hodModel.find({
+      teacher_id: body.teacherId,
+      department_id: body.departmentId,
+      year: body.year
+    }).exec();
+
+    if (checking.length > 0) {
+      return {
+        success: 0,
+        message: "Teacher has already been registered as a Head of Department."
+      };
+    }
+
+    return this.hodModel.create(body);
+  }
+
+  async viewAllHod(): Promise<any> {
+    return this.hodModel.find().exec();
+  }
+
+  async viewOneHod(id: string): Promise<any> {
+    return this.hodModel.findById(id).exec();
+  }
+
+  async updateHod(id: string, body: HodDto): Promise<any> {
+    return this.hodModel.findByIdAndUpdate(id, body).exec();
+  }
+
+  async deleteHod(id): Promise<any> {
+    return this.hodModel.findByIdAndDelete(id).exec();
+  }
+}
