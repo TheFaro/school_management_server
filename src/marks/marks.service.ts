7271d50015e4b380644ec1251e99a1e62d4ce548
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-11-19 17:46:47 +0200

    add marks services

--- src/marks/marks.service.ts
+++ src/marks/marks.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Mark } from "./schema/marks.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { MarksDto } from "./dto/marks.dto";
 
 @Injectable()
-export class MarksService {}
+export class MarksService {
+  constructor(@InjectModel(Mark.name) private readonly markModel: Model<Mark>) {
+  }
+
+  async create(body: MarksDto): Promise<any> {
+    const checking = await this.markModel.find({
+      class_student_id: body.classStudentId,
+      subject_teacher_id: body.subjectTeacherId,
+      month: body.month,
+      year: body.year
+    }).exec();
+
+    if (checking.length > 0) {
+      return {
+        success: 0,
+        message: "Mark already added. Update instead."
+      };
+    }
+
+    return this.markModel.create(body);
+  }
+
+  async viewByClassYear(classId: string, year: string): Promise<any> {
+    return this.markModel.find({ class_student_id: classId, year: year }).exec();
+  }
+
+  async update(id: string, body: MarksDto): Promise<any> {
+    return this.markModel.findByIdAndUpdate(id, body).exec();
+  }
+
+  async delete(id: string): Promise<any> {
+    return this.markModel.findByIdAndDelete(id).exec();
+  }
+}
