a533000704c4aa2a6414e9c661b0b9618ee4a709
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-12-16 00:59:35 +0200

    add classname array, dto, service and controller

--- src/classname/classname.service.ts
+++ src/classname/classname.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from "@nestjs/common";
-import { Classname } from "./schema/classname.schema";
-import { Model } from "mongoose";
-import { InjectModel } from "@nestjs/mongoose";
-import { ClassnameDto } from "./dto/classname.dto";
+import { Injectable } from '@nestjs/common';
+import { Classname } from './schema/classname.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { ClassnameDto } from './dto/classname.dto';
+// import { stringify } from 'querystring';
 
 @Injectable()
 export class ClassnameService {
-  constructor(@InjectModel(Classname.name) private readonly classnameModel: Model<Classname>) {
-  }
+  constructor(
+    @InjectModel(Classname.name)
+    private readonly classnameModel: Model<Classname>,
+  ) {}
 
   // function to add new class name
   async addNewClass(body: ClassnameDto): Promise<any> {
@@ -16,11 +19,33 @@ export class ClassnameService {
     if (checking.length > 0) {
       return {
         success: 0,
-        message: "Class already exists."
+        message: 'Class already exists.',
       };
     }
 
     return this.classnameModel.create(body);
+  }
+
+  // function to add class names from array
+  async addClassArray(classes: [ClassnameDto]): Promise<any> {
+    let created = false;
+    for (let i = 0; i < classes.length; i++) {
+      console.info(`${typeof classes[i]}`);
+      this.classnameModel.create(classes[i]);
+      if (created == false) created = true;
+    }
+
+    if (created) {
+      return {
+        success: 1,
+        message: 'Successfully registered class names.',
+      };
+    } else {
+      return {
+        success: 0,
+        message: 'Could not register class names.',
+      };
+    }
   }
 
   // function to get all classes
@@ -30,7 +55,9 @@ export class ClassnameService {
 
   // function to update classes
   async updateClassname(id: string, body: ClassnameDto): Promise<any> {
-    return this.classnameModel.findByIdAndUpdate(id, body, { new: true }).exec();
+    return this.classnameModel
+      .findByIdAndUpdate(id, body, { new: true })
+      .exec();
   }
 
   // function to delete classes
